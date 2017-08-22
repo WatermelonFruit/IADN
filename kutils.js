562fa3558b962203325fd7956b32ae269cdd5ed2
WatermelonFruit <jiakun.li@qq.com>
2017-08-22 23:33:10 +0800

    #Update kutils.js;Add Canvas入门

--- kutils.js
+++ kutils.js
@@ -68,20 +68,54 @@ function getTimeToString(date, fmt) { //原author: meizz， jk改造
   return fmt
 }
 
-  // 根据输入日期，返回当月最后一日，输入日期格式为'yyyy-MM-dd'
-  function getMonthEnd(date) {
-    var dateArr = date.split('-')
-    var monthStart = new Date(dateArr[0], dateArr[1], 1)
-    return dateArr[0] + '-' + dateArr[1] + '-' + new Date(new Date(dateArr[0], dateArr[1], 1).getTime() - 1000 * 60 * 60 * 24).getDate()
+// 'yy-MM-dd' -> 'yymmdd'
+function timeFormat2Str(str) {
+  var arr = str.split('-')
+  var newStr = ''
+  for (var i in arr) {
+    newStr += arr[i]
   }
+  return newStr
+}
+
+// 根据输入日期，返回当月最后一日，输入日期格式为'yyyy-MM-dd'
+function getMonthEnd(date) {
+  var dateArr = date.split('-')
+  var monthStart = new Date(dateArr[0], dateArr[1], 1)
+  return dateArr[0] + '-' + dateArr[1] + '-' + new Date(new Date(dateArr[0], dateArr[1], 1).getTime() - 1000 * 60 * 60 * 24).getDate()
+}
+
+/* 数组转换成字符串
+    arr 原始一维数组
+    symbol 连接符
+    quote 是否开启引号
+    例如： [1,2,4],'-',true -> "'1'-'2'-'4'"
+          [1,2,4],'-',true ->  "1-2-4"
+  */
+function arr2Str(arr, symbol, quote) {
+  var str = ''
+  for (var i in arr) {
+    if (Number(i) === arr.length - 1) {
+      quote ? str += "'" + arr[i] + "'" : str += arr[i]
+    } else {
+      quote ? str += "'" + arr[i] + "'" + symbol : str += arr[i] + symbol
+    }
+  }
+  return str
+}
+
+// strToDate('2017-04-02') -> Sun Apr 02 2017 00:00:00 GMT+0800 (中国标准时间)
+function strToDate(str) {
+  var arr = str.split('-')
+  if (arr.length === 2) {
+    return new Date(arr[0], Number(arr[1]) - 1)
+  }
+  return new Date(arr[0], Number(arr[1]) - 1, arr[2])
+}
 
 // 获得两个日期之间的所有日期(含本身)
-// 如 getDateArr('2017-03-24', '2017-04-02') -> ['2017-03-24', '2017-03-25', '2017-03-26', '2017-03-27', '2017-03-28', '2017-03-29', '2017-03-30', '2017-04-01', '2017-04-02']
-function getDateArr(start, end) {
-  function strToDate(str) {
-    var arr = str.split('-')
-    return new Date(arr[0], Number(arr[1]) - 1, arr[2])
-  }
+// 如 getDayBetween('2017-03-24', '2017-04-02') -> ['2017-03-24', '2017-03-25', '2017-03-26', '2017-03-27', '2017-03-28', '2017-03-29', '2017-03-30', '2017-04-01', '2017-04-02']
+function getDayBetween(start, end, split) {
   var startTime = strToDate(start)
   var endTime = strToDate(end)
   var dateArr = []
@@ -89,10 +123,54 @@ function getDateArr(start, end) {
     var year = startTime.getFullYear()
     var month = (startTime.getMonth() + 1).toString().length === 1 ? '0' + (startTime.getMonth() + 1).toString() : startTime.getMonth() + 1
     var day = startTime.getDate().toString().length === 1 ? '0' + startTime.getDate() : startTime.getDate()
-    dateArr.push(year + '-' + month + '-' + day)
+    dateArr.push(split ? year + '-' + month + '-' + day : year + month.toString() + day)
     startTime.setDate(startTime.getDate() + 1)
   }
   return dateArr
+}
+
+//获得两个月之间的所有月份(含本身)
+function getMonthBetween(start, end, split) {
+  var result = []
+  var s = start.split('-')
+  var e = end.split('-')
+  var min = new Date(s[0], Number(s[1]) - 1)
+  var max = new Date(e[0], Number(e[1]) - 1)
+  var curr = min
+  while (curr <= max) {
+    var month = (curr.getMonth() + 1).toString().length === 1 ? '0' + (curr.getMonth() + 1).toString() : curr.getMonth() + 1
+    result.push(split ? curr.getFullYear() + '-' + month : curr.getFullYear() + month.toString())
+    curr.setMonth(month)
+  }
+  return result
+}
+
+// 获取某日前num天日期 getBeforeDate('2017-04-02',2) -> '2017-03-31'
+function getBeforeDay(date, num) {
+  var current_date = strToDate(date).getTime()
+  var startTime = new Date(current_date - 1000 * 60 * 60 * 24 * num)
+  var year = startTime.getFullYear()
+  var month = (startTime.getMonth() + 1).toString().length === 1 ? '0' + (startTime.getMonth() + 1).toString() : startTime.getMonth() + 1
+  var day = startTime.getDate().toString().length === 1 ? '0' + startTime.getDate() : startTime.getDate()
+  return year + '-' + month + '-' + day
+}
+
+// 获取某月前num月日期 getBeforeDate('2017-04',2) -> '2017-02'
+function getBeforeMonth(date, num) {
+  var x = strToDate(date)
+  var month = x.getMonth() + 1
+  x.setMonth(month - (num + 1))
+  var start_year = x.getFullYear()
+  var start_month = (x.getMonth() + 1).toString().length === 1 ? '0' + (x.getMonth() + 1).toString() : x.getMonth() + 1
+  return start_year + '-' + start_month
+}
+
+// sql null 转化为 '--'
+function dealSqlNull(str, substring) {
+  if (typeof substring === 'number') {
+    return "CASE WHEN " + str + " IS NULL THEN CAST('--' AS VARCHAR) ELSE SUBSTRING(CAST(" + str + " AS VARCHAR),0," + substring + ") END"
+  }
+  return "CASE WHEN " + str + " IS NULL THEN CAST('--' AS VARCHAR) ELSE CAST(" + str + " AS VARCHAR) END"
 }
 
 // 将HTML转换为节点
